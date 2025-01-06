408aec5f879cef9cf3e233d42407401594d0e19f
= <=>
2025-01-06 20:12:33 +0530

    updated code and docker scripts

--- install-and-config/entrypoint-unitycatalog.sh
+++ install-and-config/entrypoint-unitycatalog.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status
 
-# Install UI dependencies and start the UI
-cd /usr/unitycatalog/unitycatalog/ui &&  
-yarn install && yarn start & 
+# Start UI
+cd /usr/unitycatalog/unitycatalog/ui && yarn install && yarn start > ui.log 2>&1 &
 
 # Start Unity Catalog Server
-cd /usr/unitycatalog/unitycatalog &&  
-./bin/start-uc-server & 
+cd /usr/unitycatalog/unitycatalog && ./bin/start-uc-server > uc-server.log 2>&1 &
+
+# Start PySpark with Jupyter
+pyspark --name "unity-catalog-pyspark-session" \
+    --master "spark://spark-master:7077" \
+    --packages "io.delta:delta-spark_2.12:3.2.1,io.unitycatalog:unitycatalog-spark_2.12:0.2.0" \
+    --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" \
+    --conf "spark.sql.catalog.spark_catalog=io.unitycatalog.spark.UCSingleCatalog" \
+    --conf "spark.sql.catalog.unity=io.unitycatalog.spark.UCSingleCatalog" \
+    --conf "spark.sql.catalog.unity.uri=http://unity-catalog:8080" \
+    --conf "spark.sql.catalog.unity.token=" \
+    --conf "spark.sql.defaultCatalog=unity" > pyspark.log 2>&1 &
 
 # Wait for background processes
 wait
--- install-and-config/entrypoint.sh
+++ install-and-config/entrypoint.sh
@@ -21,7 +21,7 @@ then
   # Start Pyspark with delta lake and unity catalog and Jupyter Lab as Pyspark environment
   pyspark --name "unity-catalog-pyspark-session" \
     --master "spark://spark-master:7077" \
-    --packages "io.delta:delta-spark_2.12:3.2.0,io.unitycatalog:unitycatalog-spark_2.12:0.2.0" \
+    --packages "io.delta:delta-spark_2.12:3.2.1,io.unitycatalog:unitycatalog-spark_2.12:0.2.0" \
     --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" \
     --conf "spark.sql.catalog.spark_catalog=io.unitycatalog.spark.UCSingleCatalog" \
     --conf "spark.sql.catalog.unity=io.unitycatalog.spark.UCSingleCatalog" \
