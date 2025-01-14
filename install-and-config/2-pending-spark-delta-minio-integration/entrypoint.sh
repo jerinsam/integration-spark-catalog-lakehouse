9a2a4a4ac2f69e756903009a41a1169f58057d81
= <=>
2025-01-14 19:02:08 +0530

    updated docker scripts

---
+++ install-and-config/2-pending-spark-delta-minio-integration/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SPARK_WORKLOAD=$1
+
+echo "SPARK_WORKLOAD: $SPARK_WORKLOAD"
+
+if [ "$SPARK_WORKLOAD" == "master" ];
+then
+  # Start Spark Master
+  start-master.sh -p 7077 > /usr/spark/delta-lake/logs/spark-master.log 2>&1 & \
+
+  # Start Jupyter Lab
+  jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root > /usr/spark/delta-lake/logs/jupyter.log 2>&1 
+
+  # # Start Pyspark with delta lake and Jupyter Lab as Pyspark environment
+  # pyspark --name "jupyter-pyspark-session" \
+  #     --master "spark://spark-master:7077" \
+  #     --packages io.delta:delta-spark_2.12:3.2.0 \
+  #     --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" \
+  #     --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" > /usr/spark/delta-lake/logs/pyspark.log 2>&1 
+
+elif [ "$SPARK_WORKLOAD" == "worker" ];
+then
+  start-worker.sh spark://spark-master:7077 > /usr/spark/delta-lake/logs/spark-worker.log 2>&1  
+elif [ "$SPARK_WORKLOAD" == "history" ]
+then
+  start-history-server.sh > /usr/spark/delta-lake/logs/spark-history-server.log 2>&1
+fi
---
+++ install-and-config/5-spark-iceberg-minio-integration/entrypoint-minio.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Start Minio Server and Console 
+minio server /usr/unitycatalog/unity-catalog-dev/data --console-address ":9001" > /usr/spark/delta-lake/logs/minio.log 2>&1 &
+
+# Keep the script running
+tail -f /dev/null
